fde794d422d2e1fa978058a9c101ccaa6c1385a7
dwipaalfarisi <dwipa.alfarisi@gmail.com>
2022-07-19 00:19:36 +0800

    next: number notation

--- hello-world.js
+++ hello-world.js
@@ -1,1 +1,11 @@
-document.writeln("hello world!")
+// for hey.html
+document.writeln('<p>hey!</p>')
+document.writeln(100)
+document.writeln('</br>')
+document.writeln(100.123)
+
+/*
+this is a comment with more
+than one line
+*/
+
